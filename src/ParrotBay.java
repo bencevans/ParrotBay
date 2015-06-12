1f279e7345ae2ddee31fa24a42be840e6e353192
Donald Ty Lam <dlamty@gmail.com>
2015-06-12 21:03:54 +0100

    last version on the finals day

--- src/ParrotBay.java
+++ src/ParrotBay.java
@@ -12,29 +12,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
+
 public class ParrotBay {
 
 	static int seq = 1;
-	static float speed = (float) 0.6
-			;
+	static float speed = (float) 0.4; //speed
 	static FloatBuffer fb;
 	static IntBuffer ib;
 	static InetAddress inet_addr;
 	static DatagramSocket socket;
 
 	public static void main(String[] args) throws Exception {
-		//1 second = 3.9 meters
 
 		//STEP 1: READ FILE & STEP 2: FIND BEST ROUTE
-		//nearestneighbourTSP(filereader());
+		QRcoords[] bestroute = convert2diff(nearestneighbourTSP(filereader()));
 
 		//STEP 3: CONNECT TO DRONE...
 		String ip = "192.168.1.1";
-
 		if (args.length >= 1) {
 			ip = args[0];
 		}
-
 		StringTokenizer st = new StringTokenizer(ip, ".");
 
 		byte[] ip_bytes = new byte[4];
@@ -58,68 +57,39 @@ public class ParrotBay {
 		socket = new DatagramSocket();
 		socket.setSoTimeout(3000);
 
-		/* altitude max 6m */
-		send_at_cmd("AT*CONFIG=1,\"control:altitude_max\",\"6000\"");
-		if (args.length == 2) { // Command line mode
+		/* altitude max 2m */
+		send_at_cmd("AT*CONFIG=1,\"control:altitude_max\",\"3000\"");
+		if (args.length == 2) 
+		{ // Command line mode
 			send_at_cmd(args[1]);
 			System.exit(0);
 		}
-		
-		//STEP 4: CALCULATE ROUTE ACTUAL MOVEMENTS
-		// take off
+
+		//STEP 4: MOVE AND READ QR
+		System.out.println("Take Off");
 		takeoff();
-
-<<<<<<< HEAD
-		forwards(2000);
-		left(2000);
-		backwards(2000); 
-		right(2000);
-
-		//		String qrValue = DisplayCameraImage.findQR(1);
-		//		System.out.println(qrValue);
-		
-		//land
-=======
-		up(500);
-		down(500);
-
->>>>>>> origin/master
+//		DisplayCameraImage.findQR(1); //This will display the camera and then reads qr code
+		move(bestroute);
 		land();
 	}
 
 	static String action;
 	static String at_cmd;
-<<<<<<< HEAD
-	
-=======
 
 
-
->>>>>>> origin/master
 	public static void up(int timeToRun) throws Exception {
 		action = "Go Up (gaz+)";
 		at_cmd = "AT*PCMD=" + (seq++) + ",1,0,0," + intOfFloat(speed)
 				+ ",0";
-		System.out.println(action);
+//		System.out.println(action);
 		send_at_cmd_time(at_cmd, timeToRun);
-<<<<<<< HEAD
 	}
-	
+
 	public static void down(int timeToRun) throws Exception {
 		action = "Go Up (gaz+)";
 		at_cmd = "AT*PCMD=" + (seq++) + ",1,0,0," + intOfFloat(-speed)
 				+ ",0";
-		System.out.println(action);
-		send_at_cmd_time(at_cmd, timeToRun);
-=======
->>>>>>> origin/master
-	}
-	
-	public static void down(int timeToRun) throws Exception {
-		action = "Go Up (gaz+)";
-		at_cmd = "AT*PCMD=" + (seq++) + ",1,0,0," + intOfFloat(-speed)
-				+ ",0";
-		System.out.println(action);
+//		System.out.println(action);
 		send_at_cmd_time(at_cmd, timeToRun);
 	}
 
@@ -127,17 +97,16 @@ public class ParrotBay {
 		action = "Hovering";
 		at_cmd = "AT*PCMD=" + (seq++) + ",1,0,0,0,0";
 		send_at_cmd(at_cmd);
-		System.out.println(action);
+//		System.out.println(action);
 		Thread.sleep(2000);
 	}
-
 
 	public static void right(int timeToRun) throws Exception {
 		String action = "Go right";
 		String at_cmd = "AT*PCMD=" + (seq++) + ",1," + intOfFloat(speed)
 				+ ",0,0,0";
 		send_at_cmd_time(at_cmd, timeToRun);
-		System.out.println(action);
+//		System.out.println(action);
 		System.out.println("seq:" + seq++);
 	}
 
@@ -146,25 +115,25 @@ public class ParrotBay {
 		String at_cmd = "AT*PCMD=" + (seq++) + ",1," + intOfFloat(-speed)
 				+ ",0,0,0";
 		send_at_cmd_time(at_cmd, timeToRun);
-		System.out.println(action);
+//		System.out.println(action);
 		System.out.println("seq:" + seq++);
 	}
-	
+
 	public static void forwards(int timeToRun) throws Exception {
 		String action = "Go forwards";
 		String at_cmd = "AT*PCMD=" + (seq++) + ",1,0," + intOfFloat(-speed)
 				+ ",0,0";
 		send_at_cmd_time(at_cmd, timeToRun);
-		System.out.println(action);
+//		System.out.println(action);
 		System.out.println("seq:" + seq++);
 	}
-	
+
 	public static void backwards(int timeToRun) throws Exception {
 		String action = "Go backwards";
 		String at_cmd = "AT*PCMD=" + (seq++) + ",1,0," + intOfFloat(speed)
 				+ ",0,0";
 		send_at_cmd_time(at_cmd, timeToRun);
-		System.out.println(action);
+//		System.out.println(action);
 		System.out.println("seq:" + seq++);
 	}
 
@@ -172,7 +141,7 @@ public class ParrotBay {
 		String action = "Takeoff";
 		String at_cmd = "AT*REF=" + (seq++) + ",290718208";
 		send_at_cmd(at_cmd);
-		System.out.println(action);
+//		System.out.println(action);
 		System.out.println("seq:" + seq++);
 		Thread.sleep(4000); // Takes ~4 seconds to take off
 	}
@@ -181,7 +150,7 @@ public class ParrotBay {
 		String action = "Landing";
 		String at_cmd = "AT*REF=" + (seq++) + ",290717696";
 		send_at_cmd(at_cmd);
-		System.out.println(action);
+//		System.out.println(action);
 		System.out.println("seq:" + seq++);
 	}
 
@@ -189,7 +158,7 @@ public class ParrotBay {
 		fb.put(0, f);
 		return ib.get(0);
 	}
-	
+
 	public static void send_at_cmd_time(String at_cmd, long timeToRun) throws Exception {
 		long startTime = System.currentTimeMillis();
 		while(System.currentTimeMillis() < startTime + timeToRun) {
@@ -216,9 +185,9 @@ public class ParrotBay {
 	{
 		Scanner coordReader;
 		ArrayList<QRcoords> List = new ArrayList<QRcoords>();
-		File readH = new File("E:\\DroneTestData.txt");
+		File readH = new File("C:\\Users\\user\\Desktop\\Test Data\\TestData.txt");
 		coordReader = new Scanner(new FileInputStream(readH));
-		
+
 		String[] lineelements = new String[4];
 		while(coordReader.hasNext()){
 			String line = coordReader.nextLine();
@@ -229,11 +198,11 @@ public class ParrotBay {
 					Double.parseDouble(lineelements[3]));		
 			List.add(set);
 		}
-			
+
 		System.out.println(List);
 		return List;
 	}
-	
+
 	/**
 	 * STEP 2: FIND BEST ROUTE
 	 * @param L	an arraylist of the QR coords as read from file input
@@ -248,129 +217,139 @@ public class ParrotBay {
 		//variables for calculations
 		double xsqdiff, ysqdiff, zsqdiff;
 
+		//starting: find the closest point to 0, 0, 0
+		double startdist[] = new double[listsize];
+		for(int i = listsize - 1; i >= 0; i--){
+			QRcoords temp = L.get(i);
+			//pythagorean calculations for distance between two QRs
+			xsqdiff = Math.pow((temp.getX() - 0), 2);
+			ysqdiff = Math.pow((temp.getY() - 0), 2);
+			zsqdiff = Math.pow((temp.getZ() - 0), 2);
+			startdist[i] = Math.sqrt(xsqdiff + ysqdiff + zsqdiff);
+		}
+		route[0] = L.get(getMin(startdist));
+		L.remove(getMin(startdist));
+		listsize = L.size();
+		System.out.println(L);
+
 		//loop: find the nearest neighbour to visit
 		for(int i = 0; listsize > 1; i++){
 			double nextdist[] = new double[listsize];
 			for(int j = 0; j < listsize; j++){
 				QRcoords temp = L.get(j);
 				//pythagorean calculations for distance between two QRs
-				if (i == 0){
-					xsqdiff = Math.pow((temp.getX() - 0), 2);
-					ysqdiff = Math.pow((temp.getY() - 0), 2);
-					zsqdiff = Math.pow((temp.getZ() - 0), 2);
-				}
-				else{
-					xsqdiff = Math.pow((route[i-1].getX() - temp.getX()), 2);
-					ysqdiff = Math.pow((route[i-1].getY() - temp.getY()), 2);
-					zsqdiff = Math.pow((route[i-1].getZ() - temp.getZ()), 2);
-				}
+				xsqdiff = Math.pow((route[i].getX() - temp.getX()), 2);
+				ysqdiff = Math.pow((route[i].getY() - temp.getY()), 2);
+				zsqdiff = Math.pow((route[i].getZ() - temp.getZ()), 2);
 				nextdist[j] = Math.sqrt(xsqdiff + ysqdiff + zsqdiff);
 			}
-			route[i] = L.get(getMin(nextdist));
+			route[i+1] = L.get(getMin(nextdist));
 			L.remove(getMin(nextdist));
 			listsize = L.size();
 			System.out.println(L);
 		}
-	
+
 		//ending: put the last coords last
 		route[route.length-1] = L.get(0);
-		
+
 		System.out.println(Arrays.toString(route));
 		return route;		
 
 	}
 	
+	//STEP 2B: convert bestroute to differences
+	public static QRcoords[] convert2diff(QRcoords[] before){
+		QRcoords[] after = new QRcoords[before.length];
+		after[0] = before[0];
+		for (int i = 1; i < before.length; i++){
+			double x, y, z, c;
+			c = before[i].getC();
+			x = before[i].getX() - before[i-1].getX();
+			y = before[i].getY() - before[i-1].getY();
+			z = before[i].getZ() - before[i-1].getZ();
+			after[i] = new QRcoords(x,y,z,c);			
+		}
+		return after;
+	}
 	/**
 	 * STEP 4: COMPUTE MOVEMENTS AND MOVE 
-	 * @param L	an arraylist of the QR coords as read from file input
+	 * @param route	an array of the QR coords as read from file input
 	 * @return  a QRcoords[] that has the coords ordered as the shortest route
 	 * @throws Exception 
 	 */
-	public static void movecalculations(QRcoords[] route) throws Exception{	
-		double xprev = 0;
-		double yprev = 0;
-		double zprev = 0;
-		double cprev = 0;
-		//using this simple conversion for now: 1 second = 3.9 meters
-		
+	public static void move(QRcoords[] route) throws Exception{	
+		double xnext = 0;
+		double ynext = 0;
+		double znext = 0;
+		double cnext = 0;
+		//using this simple conversion for now: 1 second = 2.6 meters
+
 		for (int i = 0; i < route.length; i++){
-			if (i > 0){
-			xprev = route[i-1].getX();
-			yprev = route[i-1].getY();
-			zprev = route[i-1].getZ();
-			cprev = route[i-1].getC();
-			}
-			double xnext = route[i].getX();
-			double ynext = route[i].getY();
-			double znext = route[i].getZ();
-			double cnext = route[i].getC();
+			xnext = route[i].getX();
+			ynext = route[i].getY();
+			znext = route[i].getZ();
+			cnext = route[i].getC();
 			//actual moving logic
-			if (cnext == 1){
-				//no z movement (+0.88)
-				if (xnext < 0){
-					xnext = xnext - xprev;
+			if (cnext == 1){ // bottom camera
+				//no z movement (+0.5)
+				if (xnext <= 0){
 					// move left
-					left((int)(xnext/3.9*1000));
+					System.out.println("Moving Left");
+					left((int)(xnext/2.6*1000));
 				}
 				else if (xnext > 0){
-					xnext = xnext - xprev;
 					// move right
-					right((int)(xnext/3.9*1000));
+					System.out.println("Moving Right");
+					right((int)(xnext/2.6*1000));
 				}
-				if (ynext < 0){
-					ynext = ynext - yprev;
+				if (ynext <= 0){
 					// move backward
-					backwards((int)(ynext/3.9*1000));
+					System.out.println("Moving back");
+					backwards((int)(ynext/2.6*1000));
 				}
 				else if (ynext > 0){
-					ynext = ynext - yprev;
 					// move forward
-					forwards((int)(ynext/3.9*1000));
+					System.out.println("Moving Forwards");
+					forwards((int)(ynext/2.6*1000));
 				}
+				DisplayCameraImage.findQR(1); //This will display the camera and then reads qr code
 			}
-			else if (cnext == 0){
-				if (znext > zprev){
-<<<<<<< HEAD
-					znext = znext - zprev -0.88;
+			else if (cnext == 0){ //front camera
+				if (znext >= 0){
 					//move up
-					up((int)(znext/3.9*1000));
+					System.out.println("Moving up");
+					up((int)(znext/2.6*1000));
 				}
-				else if (zprev > znext){
-					znext = znext - zprev +0.88;
+				else if (znext < 0){
 					//move down
-					down((int)(znext/3.9*1000));
-=======
-					//move z by znext = znext - zprev -0.88
+					System.out.println("Moving down");
+					down((int)(znext/2.6*1000));
 				}
-				else if (zprev > znext){
-					//move z by znext = znext - zprev +0.88
->>>>>>> origin/master
-				}
-				if (xnext < 0){
-					xnext = xnext - xprev;
+				if (xnext <= 0){
 					// move left
-					left((int)(xnext/3.9*1000));
+					System.out.println("Moving left");
+					left((int)(xnext/2.6*1000));
 				}
 				else if (xnext > 0){
-					xnext = xnext - xprev;
 					// move right
-					right((int)(xnext/3.9*1000));
+					System.out.println("Moving right");
+					right((int)(xnext/2.6*1000));
 				}
-				if (ynext < 0){
-					ynext = ynext - yprev;
+				if (ynext <= 0){
 					// move backward
-					backwards((int)(ynext/3.9*1000));
+					System.out.println("Moving backwards");
+					backwards((int)(ynext/2.6*1000));
 				}
 				else if (ynext > 0){
-					ynext = ynext - yprev;
 					// move forward
-					forwards((int)(ynext/3.9*1000));
+					System.out.println("Moving forwards");
+					forwards((int)(ynext/2.6*1000));
 				}
+				DisplayCameraImage.findQR(0); //This will display the camera and then reads qr code
 			}
 		}
-		
 	}
-		
+
 	/**
 	 * MISC: A generic factorial function
 	 */
@@ -397,5 +376,5 @@ public class ParrotBay {
 		return minVpos; 
 	} 
 
-	
+
 }
